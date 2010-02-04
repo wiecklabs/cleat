bbaaf566d8b4b3df290036f14d1a7bad2426bc30
Bernerd Schaefer <bj.schaefer@gmail.com>
2010-02-04 14:06:05 -0600

    Removing squeeze on base URL, since it turns http:// into http:/

--- lib/cleat/ui/link.rb
+++ lib/cleat/ui/link.rb
@@ -2,7 +2,7 @@ class Cleat
   module UI
     class Link
       def initialize(link, inline = false)
-        @url = "#{(Cleat::base_url + "/").squeeze("/")}#{Cleat.prefix}#{link.short_url}#{"!" if inline}"
+        @url = "#{Cleat::base_url + "/"}#{Cleat.prefix}#{link.short_url}#{"!" if inline}"
       end
 
       def to_s
